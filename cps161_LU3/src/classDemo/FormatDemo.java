bdb3f76a6146bb3b5358920be776ff1885e2c0cb
Ferdinand Lange <ferdinandlange@Ferdinands-MacBook-Pro.local>
2023-10-06 17:59:34 -0400

    LU3

--- cps161_LU3/src/classDemo/FormatDemo.java
+++ cps161_LU3/src/classDemo/FormatDemo.java
@@ -8,6 +8,7 @@ public class FormatDemo {
 		System.out.println("Hi");
 		System.out.print("\tMy name is Johny.\n");
 		System.out.println("Nice to meet you.");
+		
 		double price = 19.8;
 		String item = "chair";
 		System.out.printf("The price is: $%-6.2f for each %-12s", price, item);
